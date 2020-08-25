67bf84f27574cce312cdab7118d60bf316f32593
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-08-24 22:16:58 -0700

    add api data fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      consoles: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({consoles: users}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {
+        this.state.consoles.map(console => (
+          <h1 key={console.id}>{ console.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
