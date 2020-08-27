f3d5f7af1a20f3e527a308d703f71ebb89b19b6f
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-08-26 22:23:39 -0700

    Updates

---
+++ precache-manifest.0a2142ec86e0e724016c1d75d0ed2e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d077e156130eaf79d446a0adee3c8a22",
+    "url": "/react-rolo/index.html"
+  },
+  {
+    "revision": "4f08bebd1a9734b0e810",
+    "url": "/react-rolo/static/css/main.12987ac3.chunk.css"
+  },
+  {
+    "revision": "154189d82e8f60ef47f4",
+    "url": "/react-rolo/static/js/2.0212bbd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolo/static/js/2.0212bbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f08bebd1a9734b0e810",
+    "url": "/react-rolo/static/js/main.cb3bb8e9.chunk.js"
+  },
+  {
+    "revision": "241e2b11cde6eccb710c",
+    "url": "/react-rolo/static/js/runtime-main.d5ad4326.js"
+  }
+]);
